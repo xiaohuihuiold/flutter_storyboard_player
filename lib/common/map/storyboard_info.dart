61c30c7067eb5463dab7e15e74f934798f21eada
xiaohuihuiold <xiaohuihuiold@gmail.com>
2019-11-12 18:54:41 +0800

    添加sprite结构

--- lib/common/map/storyboard_info.dart
+++ lib/common/map/storyboard_info.dart
@@ -9,25 +9,81 @@ class OSBEvents {
   OSBBreakPeriods breakPeriods;
 
   /// 故事板图层
-  List<Sprite> layer0 = List();
-  List<Sprite> layer1 = List();
-  List<Sprite> layer2 = List();
-  List<Sprite> layer3 = List();
+  List<Sprite> backgrounds = List();
+  List<Sprite> fails = List();
+  List<Sprite> passes = List();
+  List<Sprite> foregrounds = List();
 }
 
 /// 背景
 class OSBBackground {
+  /// 背景类型:0-图片 1-视频
   int type;
+  /// 开始时间
   int startTime;
+  /// 文件
   String file;
+  /// 坐标偏移
   Offset offset;
 }
 
 /// 休息时间
 class OSBBreakPeriods {
+  /// 开始时间
   int startTime;
+  /// 结束时间
   int endTime;
 }
 
+/// 精灵类型
+enum SpriteType {
+  Sprite,
+  Animation,
+}
+
+/// 图层
+enum SpriteLayer {
+  Background,
+  Fail,
+  Pass,
+  Foreground,
+}
+
+/// 中心点
+enum SpriteOrigin {
+  TopLeft,
+  Centre,
+  CentreLeft,
+  TopRight,
+  BottomCentre,
+  TopCentre,
+  CentreRight,
+  BottomLeft,
+  BottomRight,
+}
+
+/// 动画循环类型
+enum SpriteLoopType {
+  LoopForever,
+  LoopOnce,
+}
+
 /// 精灵
-class Sprite {}
+class Sprite {
+  /// 精灵类型
+  SpriteType type;
+  /// 图层
+  SpriteLayer layer;
+  /// 中心点
+  SpriteOrigin origin;
+  /// 图片文件
+  String fileName;
+  /// 位置
+  Offset position;
+  /// 动画张数
+  int frameCount;
+  /// 动画每帧延时
+  int frameDelay;
+  /// 动画循环方式
+  SpriteLoopType loopType;
+}
